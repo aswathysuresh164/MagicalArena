d40f2ed47b1d7319043e5f0baed54bac3336e72f
aswathy164 <aswathi164@gmail.com>
2024-06-22 08:18:24 +0530

    Implimented Player.java

--- MagicalArena/src/main/java/com/swiggy/Player.java
+++ MagicalArena/src/main/java/com/swiggy/Player.java
@@ -1,7 +1,25 @@
 package com.swiggy;
 
 import lombok.Data;
+import lombok.extern.java.Log;
 
+
+/**
+ *This class represents a player in the  game
+ */
+@Log
 @Data
 public class Player {
+
+    private int health;
+    private int strength;
+    private int attack;
+
+    public boolean isAlive() {
+        return this.health > 0;
+    }
+
+
+
 }
+
